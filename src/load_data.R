111ffaca88b355390911843c1d7c132bddc5f6b3
marchellodev <marchellodev@gmail.com>
2022-06-16 19:21:21 +0300

    Clean up project files

--- src/load_data.R
+++ src/load_data.R
@@ -11,50 +11,50 @@ library("rjson")
 library("tidyverse")
 
 # Reversing so that the data is in the chronological order
-raw_data <- rev(fromJSON(file = "data.json"))
+raw_data <- rev(fromJSON(file = "data/sirens_demo.json"))
 
 # active sirens
 active_sirens <- list()
 
 all_sirens <- list()
 
-for(el in raw_data) {
+for (el in raw_data) {
   region <- str_split_fixed(el["message"], "#", 2)[2]
-  if (region == ""){
+  if (region == "") {
     next
   }
-  
+
   time <- el[["date"]]
-  
+
   message_type_raw <- substr(el["message"], 1, 1)
-  
+
   # if red circle
-  if (message_type_raw == "🔴"){
+  if (message_type_raw == "🔴") {
     # THE SIREN STARTED
-    active_entry = list(list(time=time, region=region))
+    active_entry <- list(list(time = time, region = region))
     active_sirens <- c(active_sirens, active_entry)
   }
-  
+
   # if green or yellow
-  if(message_type_raw == "🟢" || message_type_raw == "🟡"){
+  if (message_type_raw == "🟢" || message_type_raw == "🟡") {
     # THE SIREN ENDED
     i <- 1
-    for(active in active_sirens){
-      if(active["region"] == region){
+    for (active in active_sirens) {
+      if (active["region"] == region) {
         # Add the siren to the data set
-        
-        siren_entry = list(list(start=active[["time"]], end=time, region=region))
+
+        siren_entry <- list(list(start = active[["time"]], end = time, region = region))
         all_sirens <- c(all_sirens, siren_entry)
-        
+
         active_sirens[i] <- NULL
-        
+
         break
       }
-      i <- i+1
+      i <- i + 1
     }
-    
+
   }
-  
+
 }
 
 rm(active)
